c2cfc81cd11d57fadd41a69582ae7d7981642b30
Rafael Lopez <lpz.raf@gmail.com>
2020-04-18 15:45:00 -0400

    It will format the code with inital and refactored.

--- carfax-scrape.py
+++ carfax-scrape.py
@@ -10,6 +10,9 @@ def get_model_and_car(url):
 
     return 'The price for a ' + models[0].text + " is " + price[0].text[7:] + "."
 
+print(get_model_and_car('https://www.carfax.com/Used-Cars-in-Brooklyn-NY_c10456'))
+
+# Initial Code
 # def get_price(url):
 #     res = requests.get(url)
 #     res.raise_for_status()
@@ -27,5 +30,4 @@ def get_model_and_car(url):
 # car_price = get_price('https://www.carfax.com/Used-Cars-in-Brooklyn-NY_c10456')
 # car_model = get_model('https://www.carfax.com/Used-Cars-in-Brooklyn-NY_c10456')
 
-
-print(get_model_and_car('https://www.carfax.com/Used-Cars-in-Brooklyn-NY_c10456'))
+# print('The price for a ' + car_model + " is " + car_price + ".")
